af868f0aaa61014d7906e2870f3cdea99ef4d034
rikasumitomo <lmcmacf@gmail.com>
2017-09-03 10:36:18 +0900

    エラーを解消した.

--- app/src/main/java/jp/rika/sumitomo/tryexif2/MainActivity.java
+++ app/src/main/java/jp/rika/sumitomo/tryexif2/MainActivity.java
@@ -17,34 +17,45 @@ package jp.rika.sumitomo.tryexif2;
     import java.io.FileOutputStream;
     import android.widget.Toast;
 
+
 public class MainActivity extends Activity {
+
+    private ExifInterface exif;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    TextView sizeText = (TextView)findViewById(R.id.text1);
-    TextView dateText = (TextView)findViewById(R.id.text2);
-    TextView latlongText = (TextView)findViewById(R.id.text3);
-    ImageView thumbnailView = (ImageView)findViewById(R.id.image) ;
+        TextView sizeText = (TextView) findViewById(R.id.text1);
+        TextView dateText = (TextView) findViewById(R.id.text2);
+        TextView latlongText = (TextView) findViewById(R.id.text3);
+        ImageView thumbnailView = (ImageView) findViewById(R.id.image);
 
-        Drawable drawable = getResources().getDrawable(android.R.drawable.sample);
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.sample);
+        Drawable drawable = getResources().getDrawable(R.drawable.sample);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.sample);
 
+        createExternalStoragePrivateFile();
+        deleteExternalStoragePrivateFile();
+        hasExternalStoragePrivateFile();
 
 // 以下のfilenameを自分の画像のパスに変更してください。
         String filename = "/sdcard/DSC_3509.JPG";
         try {
-            ExifInterface exif = new ExifInterface(filename);
-            ShowExif(exif);
+            exif = new ExifInterface(filename);
+            //ShowExif(exif);
+            showExif();
+
+
+
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
             Toast.makeText(this, "Error!",
                     Toast.LENGTH_LONG).show();
         }
-}
-}
+
+    }
 
     void createExternalStoragePrivateFile() {
         // Create a path where we will place our private file on external
@@ -57,7 +68,7 @@ public class MainActivity extends Activity {
             // no error checking, and assumes the picture is small (does not
             // try to copy it in chunks).  Note that if external storage is
             // not currently mounted this will silently fail.
-            InputStream is = getResources().openRawResource(R.drawable.ballon);
+            InputStream is = getResources().openRawResource( + R.drawable.sample);
             OutputStream os = new FileOutputStream(file);
             byte[] data = new byte[is.available()];
             is.read(data);
@@ -89,3 +100,21 @@ public class MainActivity extends Activity {
         }
         return false;
     }
+    void showExif(){
+        if(exif != null) {
+            // get latitude and longitude
+            float[] latlong = new float[2];
+            exif.getLatLong(latlong);
+        }
+        double altitude = exif.getAttributeDouble (ExifInterface.TAG_GPS_ALTITUDE, 0); // since API Level 9
+        double altitudeRef = exif.getAttributeDouble (ExifInterface.TAG_GPS_ALTITUDE_REF, 0);
+        String latitude = exif.getAttribute (ExifInterface.TAG_GPS_LATITUDE);
+        String latitudeRef = exif.getAttribute (ExifInterface.TAG_GPS_LATITUDE_REF);
+
+
+        Log.d("exif", "altitude : " + altitude);
+        Log.d("exif", "altitudeRef : " + altitudeRef);
+        Log.d("exif", "latitude : " + latitude);
+        Log.d("exif", "latitudeRef : " + latitudeRef);
+    }
+}
